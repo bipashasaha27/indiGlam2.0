1b354aa0101ddc1490dd09b014372fd2e7960556
bipashasaha <bipashasaha544@gmail.com>
2026-02-05 02:30:07 +0530

    about page

--- JS/script.js
+++ JS/script.js
@@ -103,3 +103,33 @@ var swiper = new Swiper(".heroSwiper", {
     },
   },
 });
+
+// Coutner Code
+// Counting Numbers
+// const counters = document.querySelectorAll(".count");
+// const totalDuration = 2000; // total time for animation (2s)
+// const easeOutQuad = (t) => t * (2 - t);
+
+// function startCounters() {
+//   counters.forEach((counter) => {
+//     counter.innerText = "0 +"; // reset before starting
+//     const target = Number(counter.getAttribute("data-target")) || 0;
+//     let startTime = null;
+
+//     const updateCount = (timestamp) => {
+//       if (!startTime) startTime = timestamp;
+//       const progress = Math.min((timestamp - startTime) / totalDuration, 1);
+//       const easedProgress = easeOutQuad(progress);
+//       const current = Math.floor(target * easedProgress);
+
+//       if (progress < 1) {
+//         counter.innerText = current.toLocaleString() + " +";
+//         requestAnimationFrame(updateCount);
+//       } else {
+//         counter.innerText = formatNumber(target) + " +";
+//       }
+//     };
+
+//     requestAnimationFrame(updateCount);
+//   });
+// }
