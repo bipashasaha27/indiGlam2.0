ffd987e4c1170a6d52eb80c705f5426e9fc8d408
bipashasaha <bipashasaha544@gmail.com>
2026-02-12 22:32:18 +0530

    code added

--- JS/script.js
+++ JS/script.js
@@ -139,6 +139,19 @@ var swiper = new Swiper(".categorySlider", {
   freeModeMomentum: false,
 });
 
+// cursor point
+
+const dot = document.querySelector(".cursor-dot");
+const outline = document.querySelector(".cursor-outline");
+
+document.addEventListener("mousemove", (e) => {
+  dot.style.left = e.clientX + "px";
+  dot.style.top = e.clientY + "px";
+
+  outline.style.left = e.clientX + "px";
+  outline.style.top = e.clientY + "px";
+});
+
 // product-page-counting
 
 // const minusBtn = document.querySelector(".minus");
